3801dee2a05c8b8ef0b0f1d6992b9c1b94fe1bde
XxLaraxX <80956489+XxLaraxX@users.noreply.github.com>
2022-02-19 06:09:02 +0100

    fixes

--- Template/old/FudgeStory/FudgeStory.d.ts
+++ Template/old/FudgeStory/FudgeStory.d.ts
@@ -275,18 +275,17 @@ declare namespace FudgeStory {
      * }
      * ```
      */
-    export interface ItemDefinition {
+    interface ItemDefinition {
         name: string;
         description: string;
         image: RequestInfo;
         static?: boolean;
         handler?: (_event: CustomEvent) => void;
     }
-    const Inventory_base: any;
     /**
      * Manages the inventory
      */
-    export class Inventory extends Inventory_base {
+    class Inventory extends HTMLDialogElement {
         private static ƒDialog;
         private static ƒused;
         private static get dialog();
@@ -310,7 +309,6 @@ declare namespace FudgeStory {
         private static replaceWhitespace;
         private static getItemElement;
     }
-    export {};
 }
 declare namespace FudgeStory {
     /**
@@ -537,11 +535,10 @@ declare namespace FudgeStory {
     }
 }
 declare namespace FudgeStory {
-    const Text_base: any;
     /**
      * Displays a longer narrative text to convey larger parts of the story not told by a character
      */
-    export class Text extends Text_base {
+    class Text extends HTMLDialogElement {
         private static get dialog();
         /**
          * Prints the text in a modal dialog stylable with css
@@ -560,7 +557,6 @@ declare namespace FudgeStory {
          */
         static close(): void;
     }
-    export {};
 }
 declare namespace FudgeStory {
     /**
